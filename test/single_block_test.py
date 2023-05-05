8f3eb734bf21d739c0ffbf2764528db4d4aef2a2
Aaron Lio <lioa@PRISM-M900-3.PRISMLAB>
2023-05-05 10:31:48 -0400

    fixed typos for single_test

--- test/single_block_test.py
+++ test/single_block_test.py
@@ -6,7 +6,7 @@ full block test
 
 @author: ivanovn
 """
-
+from bcipy import bcipy
 from classes.session import Session
 from classes.tensor import Tensor
 from classes.scalar import Scalar
@@ -24,13 +24,14 @@ from random import shuffle
 
 def main():
     # create a session
-    s = Session.create()
+    s = bcipy.Session.create()
+    g = bcipy.Graph.create()
 
     # grab some data for training the classifier
     X_train, y_train = load_training_data()
         
-    X = Tensor.create_from_data(s,X_train.shape,X_train)
-    y = Tensor.create_from_data(s,y_train.shape,y_train)
+    X = bcipy.Tensor.create_from_data(s,X_train.shape,X_train)
+    y = bcipy.Tensor.create_from_data(s,y_train.shape,y_train)
 
 
     # create the volatile data source obj
@@ -40,73 +41,68 @@ def main():
     Nc = len(channels)
     label_varname_map = {0 : 'class1_trials', 1 : 'class2_trials', 2 : 'class3_trials'}
     dims = (time_samples,channels)
-    data_src = BcipMatFile.create(s,'class_trials.mat','data/',label_varname_map,dims)
+    data_src = bcipy.source.BcipMatFile.create(s,'class_trials.mat','data/',label_varname_map,dims)
     
     # create the input data tensor
-    eeg = Tensor.create_from_handle(s, (Ns,Nc), data_src)
+    eeg = bcipy.Tensor.create_from_handle(s, (Ns,Nc), data_src)
 
     # create virtual tensor (filtered data & covariance matrix)
-    t_virt = [Tensor.create_virtual(s), \
-              Tensor.create_virtual(s)]
+    t_virt = [bcipy.Tensor.create_virtual(s), \
+              bcipy.Tensor.create_virtual(s)]
 
     # create the output label
-    label = Scalar.create_from_value(s,-1)
+    label = bcipy.Scalar.create_from_value(s,-1)
     
     
     # create a filter object
     order = 4
     bandpass = (8,35) # in Hz
     fs = 250
-    f = Filter.create_butter(s,order,bandpass,btype='bandpass',fs=fs,implementation='sos')
+    f = bcipy.Filter.create_butter(s,order,bandpass,btype='bandpass',fs=fs,implementation='sos')
 
 
     # add the nodes to the block
-    CovarianceKernel.add_covariance_node(b.trial_processing_graph,t_virt[0],t_virt[1])
+    bcipy.kernels.CovarianceKernel.add_covariance_node(g,t_virt[0],t_virt[1])
     
-    FiltFiltKernel.add_filtfilt_node(b.trial_processing_graph,eeg,f,t_virt[0])
+    bcipy.kernels.FiltFiltKernel.add_filtfilt_node(g,eeg,f,t_virt[0])
     
-    RiemannMDMClassifierKernel.add_untrained_riemann_MDM_node(b.trial_processing_graph,
+    bcipy.kernels.RiemannMDMClassifierKernel.add_untrained_riemann_MDM_node(g,
                                                               t_virt[1],
                                                               label,X,y)
 
     # verify the session (i.e. schedule the nodes)
     sts = s.verify()
 
-    if sts != BcipEnums.SUCCESS:
+    if sts != bcipy.BcipEnums.SUCCESS:
         print(sts)
         print("Test Failed D=")
         return sts
-    
-    sts = s.start_block()
-    if sts != BcipEnums.SUCCESS:
-        print(sts)
-        print("Test Failed D=")
-        return sts
+
     
     trial_seq = [0]*4 + [1]*4 + [2]*4
     shuffle(trial_seq)
     
     # RUN!
-    t_num = 1
+    t_num = 0
     correct_labels = 0
-    while s.remaining_blocks != 0 and sts == BcipEnums.SUCCESS:
-        y = trial_seq[t_num-1]
-        sts = s.execute_trial(y)
+    while t_num and sts == bcipy.BcipEnums.SUCCESS:
+        print(f"t_num {t_num}, length of trials: {len(trial_seq)}")
+        y = trial_seq[t_num]
+        sts = g.execute()
         
-        if sts == BcipEnums.SUCCESS:
+        if sts == bcipy.BcipEnums.SUCCESS:
             # print the value of the most recent trial
             y_bar = label.data
-            print("Trial {}: Label = {}, Predicted label = {}".format(t_num,y,y_bar))
+            print("Trial {}: Label = {}, Predicted label = {}".format(t_num+1,y,y_bar))
             
             if y == y_bar:
                 correct_labels += 1
         
+        else:
+            print(f"Trial {t_num+1} raised error, status code: {sts}")
+            break
+
         t_num += 1
-        
-        b = s.current_block
-        if sum(b.remaining_trials()) == 0:
-            # run the block closing graph
-            s.close_block()
         
     print("Accuracy = {:.2f}%.".format(100 * correct_labels/len(trial_seq)))
     
