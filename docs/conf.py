990c3f6c76f94993c0a1351965217733de0d220c
Aaron Lio <31390441+aaronlio@users.noreply.github.com>
2023-05-31 12:57:32 -0400

    Fixed typo in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, "../../bcipy")
+    os.path.join(__file__, "../bcipy")
 ))
 
 project = 'bcipy'
