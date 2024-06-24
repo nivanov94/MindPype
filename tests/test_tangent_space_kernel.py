ceb4190a1d55c91d64bd3a1b9b3031c390fb80e5
Maddie Wong <maddie12@outlook.com>
2024-06-24 16:05:00 -0400

    Add tangent space kernel unit tests

--- tests/test_tangent_space_kernel.py
+++ tests/test_tangent_space_kernel.py
@@ -1,57 +1,41 @@
-# import mindpype as mp
-# import sys, os
-# import numpy as np
-# from pyriemann.tangentspace import TangentSpace
+import pyriemann.utils
+import pyriemann.utils.covariance
+import mindpype as mp
+import numpy as np
+import pyriemann
 
-# class TangentSpaceKernelCreationUnitTest:
-#     def __init__(self):
-#         self.__session = mp.Session.create()
-#         self.__graph = mp.Graph.create(self.__session)
+class TangentSpaceKernelUnitTest:
+    def __init__(self):
+        self.__session = mp.Session.create()
+        self.__graph = mp.Graph.create(self.__session)
 
-#     def TestTangentSpaceKernelCreation(self):
-#         inTensor = mp.Tensor.create(self.__session, (2,2,2))
-#         outTensor = mp.Tensor.create(self.__session, (2,2))
-#         node = mp.kernels.TangentSpaceKernel.add_to_graph(self.__graph,inTensor,outTensor)
-#         return node.mp_type
-    
-# class TangentSpaceKernelExecutionUnitTest:
+    def TangentSpaceKernelExecution(self, raw_data, initialization_data):
+        inTensor = mp.Tensor.create_from_data(self.__session, raw_data)
+        outTensor = mp.Tensor.create(self.__session, (2,3))
+        init_in = mp.Tensor.create_from_data(self.__session, initialization_data)
+        tensor_test_node = mp.kernels.TangentSpaceKernel.add_to_graph(self.__graph,inTensor, outTensor, initialization_data=init_in)
 
-#     def __init__(self):
-#         self.__session = mp.Session.create()
-#         self.__graph = mp.Graph.create(self.__session)
+        self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()
 
-#     def TangentSpaceKernelExecution(self):
-#         np.random.seed(44)
-#         raw_data = np.random.randint(-10,10, size=(2,2,2))
-#         inTensor = mp.Tensor.create_from_data(self.__session, raw_data)
-#         outTensor = mp.Tensor.create(self.__session, (2,2))
-    
-#         initialization_data = np.random.randint(-10,10, size=(2,2,2))
-#         init_in = mp.Tensor.create_from_data(self.__session, initialization_data)
-#         init_out = mp.Tensor.create(self.__session, (2,2))
-#         tensor_test_node = mp.kernels.TangentSpaceKernel.add_to_graph(self.__graph,inTensor, outTensor, initialization_data=init_in)
+        return outTensor.data
 
-#         sys.stdout = open(os.devnull, 'w')
-#         self.__graph.verify()
-#         self.__graph.initialize(init_in, init_out)
-#         self.__graph.execute()
-#         sys.stdout = sys.__stdout__
+def test_execute():
+    np.random.seed(44)
+    raw_data = np.random.randn(2,2,2)
+    raw_data = pyriemann.utils.covariance.covariances(raw_data)
+    r = 0.001
+    raw_data = (1-r)*raw_data + r*np.diag(np.ones(raw_data.shape[-1]))
+    initialization_data = np.random.randn(2,2,2)
+    initialization_data = pyriemann.utils.covariance.covariances(initialization_data)
+    initialization_data = (1-r)*initialization_data + r*np.diag(np.ones(initialization_data.shape[-1]))
+    KernelExecutionUnitTest_Object = TangentSpaceKernelUnitTest()
+    res = KernelExecutionUnitTest_Object.TangentSpaceKernelExecution(raw_data, initialization_data)
 
-#         return (inTensor.data, init_in.data, outTensor.data)
+    tangent_space = pyriemann.tangentspace.TangentSpace()
+    tangent_space.fit(initialization_data)
+    output = tangent_space.transform(raw_data)
+    assert (res == output).all()
 
-# def test_create():
-#     KernelUnitTest_Object = TangentSpaceKernelCreationUnitTest()
-#     assert KernelUnitTest_Object.TestTangentSpaceKernelCreation() == mp.MPEnums.NODE
-#     del KernelUnitTest_Object
-
-
-# def test_execute():
-#     KernelExecutionUnitTest_Object = TangentSpaceKernelExecutionUnitTest()
-#     res = KernelExecutionUnitTest_Object.TangentSpaceKernelExecution()
-
-#     tangent_space = TangentSpace()
-#     tangent_space.fit(res[1])
-#     output = tangent_space.transform(res[0])
-#     assert (res[2] == output).all()
-
-#     del KernelExecutionUnitTest_Object
+    del KernelExecutionUnitTest_Object
