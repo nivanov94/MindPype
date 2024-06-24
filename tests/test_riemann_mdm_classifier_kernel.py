e89c3eb604e2c9c227c81ecdf18551996d8bf37d
Maddie Wong <maddie12@outlook.com>
2024-06-24 16:16:57 -0400

    fix mdm kernel unit test bug

--- tests/test_riemann_mdm_classifier_kernel.py
+++ tests/test_riemann_mdm_classifier_kernel.py
@@ -10,15 +10,8 @@ class RiemannMDMKernelUnitTest:
         self.__graph = mp.Graph.create(self.__session)
 
     def TestRiemannMDMKernelExecution(self, raw_data, init_data, init_label_data):
-        raw_data = pyriemann.utils.covariance.covariances(raw_data)
-        # apply regularization to ensure SPD matrices
-        r = 0.001
-        raw_data = (1-r)*raw_data + r*np.diag(np.ones(raw_data.shape[-1]))
         inTensor = mp.Tensor.create_from_data(self.__session, raw_data)
-        outTensor = mp.Tensor.create(self.__session, (10))
-        init_data = pyriemann.utils.covariance.covariances(init_data)
-        # apply regularization
-        init_data = (1-r)*init_data + r*np.diag(np.ones(init_data.shape[-1]))
+        outTensor = mp.Tensor.create(self.__session, (10,))
         init_inputs = mp.Tensor.create_from_data(self.__session, init_data)
         labels = mp.Tensor.create_from_data(self.__session, init_label_data)
         node = mp.kernels.RiemannMDMClassifierKernel.add_to_graph(self.__graph,inTensor,outTensor,num_classes=2,initialization_data=init_inputs,labels=labels)
@@ -32,7 +25,12 @@ class RiemannMDMKernelUnitTest:
 def test_execute():
     np.random.seed(42)
     raw_data = np.random.randn(10,10,10)
+    raw_data = pyriemann.utils.covariance.covariances(raw_data)
+    r = 0.001
+    raw_data = (1-r)*raw_data + r*np.diag(np.ones(raw_data.shape[-1]))
     init_data = np.random.randn(10,10,10)
+    init_data = pyriemann.utils.covariance.covariances(init_data)
+    init_data = (1-r)*init_data + r*np.diag(np.ones(init_data.shape[-1]))
     init_label_data = np.random.randint(0,2, size=(10,))
     
     KernelExecutionUnitTest_Object = RiemannMDMKernelUnitTest()
