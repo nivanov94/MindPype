8807538565328b1a8c8659324615fb3533520cf1
maddiew22 <112222595+maddiew22@users.noreply.github.com>
2024-07-04 13:23:50 -0400

    Update mindpype/containers.py

--- mindpype/containers.py
+++ mindpype/containers.py
@@ -672,7 +672,7 @@ class Tensor(MPBase):
         if self.virtual:
             self.shape = shape
             # when changing the shape write a zero tensor to data
-            self.data = np.zeros(shape)
+            self._data = np.zeros(shape)
         else:
             raise ValueError("Cannot change shape of non-virtual tensor")
 
