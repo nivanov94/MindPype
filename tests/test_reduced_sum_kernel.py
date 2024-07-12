Correct output size calculation for reduced sum kernel.

tests/test_reduced_sum_kernel.py
@@ -8,23 +8,23 @@ class ReducedSumKernelUnitTest:
 
     def TestReducedSumKernelExecution(self, raw_data, ax, keep_dims):
         inTensor = mp.Tensor.create_from_data(self.__session, raw_data)
+        
         # compute outTensor shape
-        if ax != None:
-            axis = (ax,)
+        input_sz = inTensor.shape
+        if ax is not None:
+            reduced_axes = [a==ax for a in range(len(input_sz))]
         else:
-            axis = ()
-        out_shape = None
-        if keep_dims:
-            # all reduced dimensions will be '1'
-            out_shape = tuple([1 if i in axis else inTensor.shape[i]
-                                          for i in range(len(inTensor.shape))])
-        elif axis == ():
-            out_shape = (1,)
-        else:
-            out_shape = tuple([inTensor.shape[i] for i in range(len(inTensor.shape))
-                                                   if i not in axis])
+            reduced_axes = [True] * len(input_sz)
+
+        output_sz = []
+        for i in range(len(input_sz)):
+            if reduced_axes[i] and keep_dims:
+                output_sz.append(1)
+            elif not reduced_axes[i]:
+                output_sz.append(input_sz[i])
+
             
-        outTensor = mp.Tensor.create(self.__session, out_shape)
+        outTensor = mp.Tensor.create(self.__session, output_sz)
         tensor_test_node = mp.kernels.ReducedSumKernel.add_to_graph(self.__graph,inTensor,outTensor,axis=ax,keep_dims=keep_dims)
 
         self.__graph.verify()
@@ -37,7 +37,7 @@ def test_execute():
     np.random.seed(44)
     raw_data = np.random.randn(2,2,2)
     axis = None
-    keep_dims = False
+    keep_dims = True
     KernelExecutionUnitTest_Object = ReducedSumKernelUnitTest()
     res = KernelExecutionUnitTest_Object.TestReducedSumKernelExecution(raw_data, axis, keep_dims)
     expected_output = np.sum(raw_data, axis = None)