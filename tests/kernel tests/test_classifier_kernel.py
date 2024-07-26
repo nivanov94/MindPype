cdba797864d934a623581c7ca3500927285ffa35
Maddie Wong <maddie12@outlook.com>
2024-07-26 14:46:58 -0400

    more error unit tests for classifier kernel

--- tests/kernel tests/test_classifier_kernel.py
+++ tests/kernel tests/test_classifier_kernel.py
@@ -8,7 +8,7 @@ class ClassifierKernelUnitTest:
         self.__session = mp.Session.create()
         self.__graph = mp.Graph.create(self.__session)
 
-    def TestClassifierKernelExecution(self, raw_data, init_data, init_labels_data, test_invalid_input=False, test_invalid_classifiers=False):
+    def TestClassifierKernelExecution(self, raw_data, init_data, init_labels_data, num_classes=4, test_invalid_input=False, test_invalid_classifiers=False):
         if test_invalid_input:
             inTensor = mp.Scalar.create_from_value(self.__session, "test")
         else:
@@ -17,15 +17,17 @@ class ClassifierKernelUnitTest:
             mp_clsf = mp.Scalar.create_from_value(self.__session, "test")
         else:
             mp_clsf = mp.Classifier.create_LDA(self.__session, shrinkage='auto', solver='lsqr')
+ 
         predictions = mp.Tensor.create(self.__session, init_labels_data.shape)
         init_tensor = mp.Tensor.create_from_data(self.__session, init_data)
         init_labels = mp.Tensor.create_from_data(self.__session, init_labels_data)
-        node = mp.kernels.ClassifierKernel.add_to_graph(self.__graph,inTensor,mp_clsf,predictions,num_classes=4,initialization_data=init_tensor,labels=init_labels)
+        node = mp.kernels.ClassifierKernel.add_to_graph(self.__graph,inTensor,mp_clsf,predictions,num_classes=num_classes,initialization_data=init_tensor,labels=init_labels)
         self.__graph.verify()
         self.__graph.initialize(init_tensor, init_labels)
         self.__graph.execute()
         return predictions.data
 
+    
 def test_execute():
     np.random.seed(44)
     raw_data = np.random.randn(50,26)
@@ -49,3 +51,11 @@ def test_execute():
         res = KernelExecutionUnitTest_Object.TestClassifierKernelExecution(raw_data, init_data, init_labels_data, test_invalid_classifiers=True)
     del KernelExecutionUnitTest_Object
     
+    init_data = np.random.randn(2,2)
+    init_labels_2d = np.random.randint(0,4, (2,2))
+    KernelExecutionUnitTest_Object = ClassifierKernelUnitTest()
+    with pytest.raises(ValueError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestClassifierKernelExecution(raw_data, init_data, init_labels_2d)
+    del KernelExecutionUnitTest_Object
+    
+test_execute()
