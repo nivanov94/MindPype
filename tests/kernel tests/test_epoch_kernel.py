ac555e8b69c53ad81016ade2fb47917857708302
Maddie Wong <maddie12@outlook.com>
2024-07-26 13:43:41 -0400

    Add unit tests for epoch kernel errors

--- tests/kernel tests/test_epoch_kernel.py
+++ tests/kernel tests/test_epoch_kernel.py
@@ -1,6 +1,7 @@
 
 import mindpype as mp
 import numpy as np
+import pytest
 
 class EpochKernelUnitTest:
     def __init__(self):
@@ -20,7 +21,31 @@ class EpochKernelUnitTest:
         self.__graph.initialize()
         self.__graph.execute()
         return outTensor.data
-
+    
+    def TestNonTensorInput(self, epoch_length, epoch_stride, ax):
+        input = mp.Scalar.create_from_value(self.__session, "test")
+        output = mp.Scalar.create(self.__session, str)
+        node = mp.kernels.EpochKernel.add_to_graph(self.__graph,input,output,epoch_len=epoch_length, epoch_stride=epoch_stride, axis=ax)
+        self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()
+    
+    def TestNonTensorOutput(self, raw_data, epoch_length, epoch_stride, ax):
+        input = mp.Tensor.create_from_data(self.__session, raw_data)
+        output = mp.Scalar.create(self.__session, str)
+        node = mp.kernels.EpochKernel.add_to_graph(self.__graph,input,output,epoch_len=epoch_length, epoch_stride=epoch_stride, axis=ax)
+        self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()    
+        
+    def TestWrongOutputShape(self, raw_data, epoch_length, epoch_stride, ax):
+        input = mp.Tensor.create_from_data(self.__session, raw_data)
+        output = mp.Tensor.create(self.__session, shape=(100,100))
+        node = mp.kernels.EpochKernel.add_to_graph(self.__graph,input,output,epoch_len=epoch_length, epoch_stride=epoch_stride, axis=ax)
+        self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()  
+        
 def test_execute():
     np.random.seed(44)
     raw_data = np.random.randn(2,2,2)
@@ -42,4 +67,29 @@ def test_execute():
         expected_output[tuple(dst_slc)] = raw_data[tuple(src_slc)]
 
     assert (res == expected_output).all()
+    
+    with pytest.raises(TypeError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestNonTensorInput(epoch_length, epoch_stride, axis)
+    del KernelExecutionUnitTest_Object
+    
+    KernelExecutionUnitTest_Object = EpochKernelUnitTest()    
+    with pytest.raises(TypeError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestNonTensorOutput(raw_data, epoch_length, epoch_stride, axis)
+    del KernelExecutionUnitTest_Object
+    
+    # test invalid epoch length
+    KernelExecutionUnitTest_Object = EpochKernelUnitTest()    
+    with pytest.raises(ValueError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestEpochKernelExecution(raw_data, epoch_length=0, epoch_stride=epoch_stride, ax=axis)
+    del KernelExecutionUnitTest_Object
+    
+    # test invalid epoch stride
+    KernelExecutionUnitTest_Object = EpochKernelUnitTest()    
+    with pytest.raises(ValueError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestEpochKernelExecution(raw_data, epoch_length=epoch_length, epoch_stride=-1, ax=axis)
+    del KernelExecutionUnitTest_Object
+    
+    KernelExecutionUnitTest_Object = EpochKernelUnitTest()    
+    with pytest.raises(ValueError) as e_info:
+        res = KernelExecutionUnitTest_Object.TestWrongOutputShape(raw_data, epoch_length=epoch_length, epoch_stride=epoch_stride, ax=axis)
     del KernelExecutionUnitTest_Object
