3cf42558c0ef8ad8bd90f8cd0a43fbacfbb0e4b2
Maddie Wong <maddie12@outlook.com>
2024-07-30 12:00:44 -0400

    add initialization data to covariance kernel in mdm pipeline

--- tests/test_MDM_pipeline.py
+++ tests/test_MDM_pipeline.py
@@ -5,7 +5,7 @@ import mindpype as mp
 import numpy as np
 from scipy import signal
 
-class MDMPipelinenitTest:
+class MDMPipelineUnitTest:
     def __init__(self):
         self.__session = mp.Session.create()
         self.__graph = mp.Graph.create(self.__session)
@@ -15,7 +15,6 @@ class MDMPipelinenitTest:
         labels = mp.Tensor.create_from_data(self.__session, labels)
         
         inTensor = mp.Tensor.create_from_data(self.__session, input_data)
-        # outTensor = mp.Scalar.create_from_value(self.__session,-1)
         outTensor = mp.Tensor.create(self.__session, shape=(inTensor.shape[0],))
         
         virtual_tensors = [
@@ -30,8 +29,8 @@ class MDMPipelinenitTest:
         filter = mp.Filter.create_butter(self.__session,order,bandpass,btype='bandpass',fs=fs,implementation='sos')
 
         node1 = mp.kernels.FilterKernel.add_to_graph(self.__graph,inTensor, filter, virtual_tensors[0])
-        node2 = mp.kernels.CovarianceKernel.add_to_graph(self.__graph,virtual_tensors[0],virtual_tensors[1])
-        node3 = mp.kernels.RiemannMDMClassifierKernel.add_to_graph(self.__graph,virtual_tensors[1],outTensor,num_classes=3, initialization_data=training_data,labels=labels)
+        node2 = mp.kernels.CovarianceKernel.add_to_graph(self.__graph,virtual_tensors[0],virtual_tensors[1], regularization=0.001, init_input=training_data, init_labels=labels)
+        node3 = mp.kernels.RiemannMDMClassifierKernel.add_to_graph(self.__graph,virtual_tensors[1],outTensor,num_classes=3)
         
         self.__graph.verify()
         self.__graph.initialize()
@@ -41,27 +40,27 @@ class MDMPipelinenitTest:
 
 def test_execute():
     # create fake initialization and input data
-
-    raw_training_data = np.random.normal(loc=0.0,scale=1.0,size=(180,250,12))
-    training_data = np.zeros((180,12,12))
-    for i in range(180):
-        training_data[i,:,:] = np.cov(raw_training_data[i,:,:],rowvar=False)
+    training_data = np.random.rand(180,12,12)
     
     labels = np.asarray([0]*60 + [1]*60 + [2]*60)
         
     input_data = np.random.randn(180,12,12)
     
-    KernelExecutionUnitTest_Object = MDMPipelinenitTest()
+    KernelExecutionUnitTest_Object = MDMPipelineUnitTest()
     res = KernelExecutionUnitTest_Object.TestMDMPipelineExecution(input_data, training_data, labels)
     
     # manually perform operations
     filter = signal.butter(4,(8,35),btype='bandpass',output='sos',fs=250)
     filtered_data = signal.sosfilt(filter,input_data,axis=1)
+    r = 0.001
     cov_data = pyriemann.utils.covariance.covariances(filtered_data)
-
+    cov_data = (1-r)*cov_data + r*np.diag(np.ones(cov_data.shape[-1]))
+    cov_init_data = pyriemann.utils.covariance.covariances(training_data)
+    cov_init_data = (1-r)*cov_init_data + r*np.diag(np.ones(cov_init_data.shape[-1]))
     model = pyriemann.classification.MDM()
-    model.fit(training_data, labels)
+    model.fit(cov_init_data, labels)
     expected_output = model.predict(cov_data)
     
     assert(res == expected_output).all()
     del KernelExecutionUnitTest_Object
+test_execute()
