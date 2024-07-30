74733fd51db0589bfa79b6e704af7fe97d8d0738
Maddie Wong <maddie12@outlook.com>
2024-07-30 15:29:34 -0400

    create a feature selection classifier pipeline unit test

--- tests/kernel tests/test_misc_graphs.py
+++ tests/kernel tests/test_misc_graphs.py
@@ -2,6 +2,8 @@ import mindpype as mp
 import numpy as np
 from pyriemann.estimation import XdawnCovariances
 from scipy import signal
+from sklearn.feature_selection import SelectKBest
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 class MiscPipelineUnitTest:
     def __init__(self):
@@ -100,7 +102,29 @@ class Misc4PipelineUnitTest():
 
         return outTensor.data
     
+class Misc5PipelineUnitTest():
+    def __init__(self):
+        self.__session = mp.Session.create()
+        self.__graph = mp.Graph.create(self.__session)
+
+    def TestMisc5PipelineExecution(self, input_data, init_data, init_label_data):    
+        init_data = mp.Tensor.create_from_data(self.__session, init_data)
+        init_labels = mp.Tensor.create_from_data(self.__session, init_label_data)
+        inTensor = mp.Tensor.create_from_data(self.__session, input_data)
+        outTensor = mp.Tensor.create(self.__session, (inTensor.shape[0],))
         
+        virtual_tensors = [
+            mp.Tensor.create_virtual(self.__session),
+        ]
+        mp_clsf = mp.Classifier.create_LDA(self.__session, shrinkage='auto', solver='lsqr')
+        node1 = mp.kernels.FeatureSelectionKernel.add_to_graph(self.__graph,inTensor, virtual_tensors[0], init_inputs=init_data, labels=init_labels)
+        node2 = mp.kernels.ClassifierKernel.add_to_graph(self.__graph, virtual_tensors[0], mp_clsf, outTensor)
+
+        self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()
+
+        return outTensor.data        
     
 def test_execute():
     np.random.seed(44)
@@ -151,6 +175,21 @@ def test_execute():
     xdawn_estimator = XdawnCovariances()
     xdawn_estimator.fit(resampled_init, init_label_data)
     expected_output = xdawn_estimator.transform(output1)
+    assert (res == expected_output).all()
+    del KernelExecutionUnitTest_Object
+    
+    KernelExecutionUnitTest_Object = Misc5PipelineUnitTest()
+    raw_data = np.random.randn(50,26)
+    init_data = np.random.randn(50,26)
+    init_labels_data = np.random.randint(0,4, (50,))
+    res = KernelExecutionUnitTest_Object.TestMisc5PipelineExecution(raw_data, init_data, init_labels_data)
+    model = SelectKBest(k=10)
+    model.fit(init_data, init_labels_data)
+    init_data = model.transform(init_data)
+    output1 = model.transform(raw_data)
+    classifier = LinearDiscriminantAnalysis(shrinkage='auto', solver='lsqr')
+    classifier.fit(init_data, init_labels_data)
+    expected_output = classifier.predict(output1)
     assert (res == expected_output).all()
     del KernelExecutionUnitTest_Object
 test_execute()
