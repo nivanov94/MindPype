593a65934dfbd104fc88e69264ccf571d7c1cf36
Maddie Wong <maddie12@outlook.com>
2024-08-09 11:24:03 -0400

    add unit test for find_obj func

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,22 +2,32 @@ import mindpype as mp
 import numpy as np
 import pickle
 
-class SaveSessionUnitTest():
+class CoreUnitTest():
     def __init__(self):
         self.__session = mp.Session.create()
     
-    def TestSaveSessionExecution(self):   
+    def TestFindObjFunc(self, raw_data):
+        obj1 = mp.Tensor.create_from_data(self.__session, raw_data)
+        obj2 = mp.Scalar.create_from_value(self.__session, 'test')
+        obj = self.__session.find_obj(obj1._id)
+        return obj
+    
+    def TestSaveSessionFunc(self):   
         obj1 = mp.Tensor.create_from_data(self.__session, np.zeros((3,3,3)))
         obj2 = mp.Scalar.create_from_value(self.__session, 'test')
         output = self.__session.save_session(file='test.pickle')
         return output
     
 def test_execute():
-    Test = SaveSessionUnitTest()
-    res = Test.TestSaveSessionExecution()
+    Test = CoreUnitTest()
+    res = Test.TestSaveSessionFunc()
     with open("test.pickle", "rb") as f:
         x = pickle.load(f)
     
     assert isinstance(res['pipeline'], mp.Session)
+    
+    raw_data = np.zeros((3,3,3))
+    res = Test.TestFindObjFunc(raw_data)
+    assert (res.data == raw_data).all()
     
 test_execute()
