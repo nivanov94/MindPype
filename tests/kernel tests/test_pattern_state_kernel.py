190b2c3dc5bde88e2146c987edefb4e7e9d8b286
Samantha Schaus <samantha.schaus11@gmail.com>
2025-06-27 12:23:47 -0400

    codecov test

--- tests/kernel tests/test_pattern_state_kernel.py
+++ tests/kernel tests/test_pattern_state_kernel.py
@@ -1,10 +1,24 @@
-import mindpype as mp
-import numpy as np
+# import mindpype as mp
+# import numpy as np
 
-class PatternStateKernelTest:
-    def __init__(self):
-        self.__session = mp.Session.create()
-        self.__graph = mp.Graph.create(self.__session)
+# class PatternStateKernelTest:
+#     def __init__(self):
+#         self.__session = mp.Session.create()
+#         self.__graph = mp.Graph.create(self.__session)
 
-    def TestKernelCreation(self, raw_data):
-        inA = mp.Tensor.create_from_data(self.__session, raw_data)
+#     def TestKernelCreation(self, raw_data):
+#         inA = mp.Tensor.create_from_data(self.__session, raw_data)
+#         outA = mp.Scalar.create(self.__session, int)
+#         outB = mp.Tensor.create(self.__session, (1,4))
+#         node = mp.kernels.PatternStateKernel.add_to_graph(self.__graph, inA, outA, outB)
+
+#         self.__graph.verify()
+#         self.__graph.initialize()
+#         self.__graph.execute()
+
+# def test_execute():
+#     t = PatternStateKernelTest()
+#     raw_data = np.ones((1,1))
+#     t.TestKernelCreation(raw_data)
+
+# test_execute()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,41 +1,41 @@
-import mindpype as mp
-import numpy as np
-import pickle
-import pytest
+# import mindpype as mp
+# import numpy as np
+# import pickle
+# import pytest
 
-class CoreUnitTest():
-    def __init__(self):
-        self.__session = mp.Session.create()
+# class CoreUnitTest():
+#     def __init__(self):
+#         self.__session = mp.Session.create()
     
-    def TestFindObjFunc(self, raw_data):
-        obj1 = mp.Tensor.create_from_data(self.__session, raw_data)
-        obj2 = mp.Scalar.create_from_value(self.__session, 'test')
-        obj = self.__session.find_obj(obj1.session_id)
-        return obj
+#     def TestFindObjFunc(self, raw_data):
+#         obj1 = mp.Tensor.create_from_data(self.__session, raw_data)
+#         obj2 = mp.Scalar.create_from_value(self.__session, 'test')
+#         obj = self.__session.find_obj(obj1.session_id)
+#         return obj
     
-    def TestSaveSessionFunc(self):   
-        obj1 = mp.Tensor.create_from_data(self.__session, np.zeros((3,3,3)))
-        obj2 = mp.Scalar.create_from_value(self.__session, 'test')
-        output = self.__session.save_session(file='test.pickle')
-        return output
+#     def TestSaveSessionFunc(self):   
+#         obj1 = mp.Tensor.create_from_data(self.__session, np.zeros((3,3,3)))
+#         obj2 = mp.Scalar.create_from_value(self.__session, 'test')
+#         output = self.__session.save_session(file='test.pickle')
+#         return output
     
-    def TestAddToSessionError(self):
-        test_string = 'test'
-        self.__session.add_to_session(test_string)
+#     def TestAddToSessionError(self):
+#         test_string = 'test'
+#         self.__session.add_to_session(test_string)
     
-def test_execute():
-    Test = CoreUnitTest()
-    res = Test.TestSaveSessionFunc()
-    with open("test.pickle", "rb") as f:
-        x = pickle.load(f)
+# def test_execute():
+#     Test = CoreUnitTest()
+#     res = Test.TestSaveSessionFunc()
+#     with open("test.pickle", "rb") as f:
+#         x = pickle.load(f)
     
-    assert isinstance(res['pipeline'], mp.Session)
+#     assert isinstance(res['pipeline'], mp.Session)
     
-    raw_data = np.zeros((3,3,3))
-    res = Test.TestFindObjFunc(raw_data)
-    assert (res.data == raw_data).all()
+#     raw_data = np.zeros((3,3,3))
+#     res = Test.TestFindObjFunc(raw_data)
+#     assert (res.data == raw_data).all()
     
-    with pytest.raises(ValueError) as e_info:
-        res = Test.TestAddToSessionError()
+#     with pytest.raises(ValueError) as e_info:
+#         res = Test.TestAddToSessionError()
     
-test_execute()
+# test_execute()
