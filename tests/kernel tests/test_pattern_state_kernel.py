b75cf357d7cfd56ebedffde3595d7fcae037396f
Samantha Schaus <samantha.schaus11@gmail.com>
2025-06-30 15:37:18 -0400

    fixed error

--- tests/kernel tests/test_pattern_state_kernel.py
+++ tests/kernel tests/test_pattern_state_kernel.py
@@ -6,11 +6,11 @@ class PatternStateKernelTest:
         self.__session = mp.Session.create()
         self.__graph = mp.Graph.create(self.__session)
 
-    def TestKernelCreation(self, raw_data, init_data):
+    def TestKernelCreation(self, raw_data):
         inA = mp.Tensor.create_from_data(self.__session, raw_data)
         outA = mp.Scalar.create(self.__session, int)
         outB = mp.Tensor.create(self.__session, (1,4))
-        node = mp.kernels.PatternStateKernel.add_to_graph(self.__graph, inA, outA, outB, init_data=)
+        node = mp.kernels.PatternStateKernel.add_to_graph(self.__graph, inA, outA, outB)
 
         self.__graph.verify()
         self.__graph.initialize()
@@ -18,8 +18,7 @@ class PatternStateKernelTest:
 
 def test_execute():
     t = PatternStateKernelTest()
-    raw_data = np.ones((1,1))
-    init_data = 
-    t.TestKernelCreation(raw_data, init_data)
+    raw_data = np.ones((1,1)) 
+    t.TestKernelCreation(raw_data)
 
 test_execute()
