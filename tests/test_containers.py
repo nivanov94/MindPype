15896914ed6afb1db459981e45cd466caff68ee0
Samantha Schaus <samantha.schaus11@gmail.com>
2025-07-02 09:45:51 -0400

    coverage

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -22,15 +22,15 @@ class ScalarUnitTests:
             print("Proper error")
 
         try:
-            val_scalar = mp.Scalar.create_from_value(self.__session, np.double(4.2))  ## line 436
+            val_scalar = mp.Scalar.create_from_value(self.__session, np.double(4.2)) 
         except TypeError:
             print("Proper error")
         
     def TestScalarData(self):
         scal = mp.Scalar.create(self.__session, int)
-        scal.data = np.array([1])   ## line 197
+        scal.data = np.array([1])   
         try:
-            scal.data = np.array([1,2]) # line 199
+            scal.data = np.array([1,2]) 
         except ValueError:
             print("Proper error")
         scal.data = np.float16(2) 
@@ -45,7 +45,7 @@ class ScalarUnitTests:
         rand_scal1 = mp.Scalar.create(self.__session, complex)
         rand_scal2 = mp.Scalar.create(self.__session, bool)
         rand_scal1.assign_random_data()
-        rand_scal2.assign_random_data()    ## all lines 213,216,217
+        rand_scal2.assign_random_data()    
 
     def TestCopyTo(self):
         scal = mp.Scalar.create(self.__session, int)
@@ -74,63 +74,93 @@ class TensorUnitTests:
             print("Proper error")
 
         try:
-            t.data = np.ndarray([1, 2, 3])  ## line 667
+            t.data = np.ndarray([1, 2, 3])
         except ValueError:
             print("Proper error")
     
     def TestChangeShape(self):
         t = mp.Tensor.create(self.__session, (1,1))
         try:    
-            t.change_shape(1)   ## line 702
+            t.change_shape(1)   
         except TypeError:
             print("Proper error")
 
     def TestTensorRandomData(self):
         tensor = mp.Tensor.create(self.__session, (1, 1, 1, 1)) 
         try:
-            tensor.assign_random_data(covariance=True)   ## line 802
+            tensor.assign_random_data(covariance=True) 
         except ValueError:
             print("Proper error")
 
         tensor1 = mp.Tensor.create(self.__session, (1, 2, 3))
         try:
-            tensor1.assign_random_data(covariance=True)   ## line 808
+            tensor1.assign_random_data(covariance=True)   
         except ValueError:
             print("Proper error")
 
         tensor2 = mp.Tensor.create(self.__session, (3,3))
-        tensor2.assign_random_data(covariance=True)   ## line 814
+        tensor2.assign_random_data(covariance=True) 
 
     def TestCreateFromData(self):
-        tensor = mp.Tensor.create_from_data(self.__session, [1,2,3,3])   ## line 934
+        tensor = mp.Tensor.create_from_data(self.__session, [1,2,3,3])   
 
 class ArrayUnitTests:
     def __init__(self):
         self.__session = mp.Session.create()
         self.__graph = mp.Graph.create(self.__session)
-        
+
+    def TestArrayGetElement(self):
+        arr = mp.Array.create(self.__session, 4, mp.Scalar.create(self.__session, int))
+        arr.get_element(-1) #3 line 1077
+        try:
+            arr.get_element(5)  ## 1080
+        except ValueError:
+            print("Proper error")
+
     def TestArraySetElement(self):
         arr = mp.Array.create(self.__session, 4, mp.Scalar.create(self.__session, int))
         try:
-            arr.set_element(5, 6)   ## line 952
+            arr.set_element(-1, mp.Scalar.create(self.__session, int))   ## line 1111
+        except ValueError:
+            print("Proper error")
+
+        try:
+            arr.set_element(5, mp.Scalar.create(self.__session, int))   ## line 1114
         except ValueError:
             print("Proper error")
 
     def TestArrayNumElements(self):
         arr1 = mp.Array.create(self.__session, 4, mp.Scalar.create(self.__session, int))
-        # num = arr1.num_elements()   ## line 965
+        # num = arr1.num_elements()   ## line 1124
+
+    def TestCopyTo(self):
+        a = mp.Array.create(self.__session, 4, mp.Scalar.create(self.__session, int))
+        dest1 = mp.Array.create(self.__session, 4, mp.Scalar.create(self.__session, int))
+        dest2 = mp.Array.create(self.__session, 5, mp.Scalar.create(self.__session, int))
+        dest3 = mp.Array.create(self.__session, 4, mp.Tensor.create(self.__session, (1,1)))
+
+        a.copy_to(dest1)
+        
+        try:
+            a.copy_to(dest2)
+        except ValueError:
+            print("Proper error")
+
+        try:
+            a.copy_to(dest3)
+        except TypeError:
+            print("Proper error")
 
     def TestArrayToTensor(self):
         arr2 = mp.Array.create(self.__session, 6, mp.Scalar.create(self.__session, int))
         t = arr2.to_tensor()
-        print(type(t))
         # assert t.mp_type == MPEnums.TENSOR        ## what type should this be
 
         arr3 = mp.Array.create(self.__session, 6, mp.Scalar.create(self.__session, bool))
-        t = arr3.to_tensor()
-
-        # assert t == None        ## why isn't this working
-
+        try:
+            t = arr3.to_tensor()   ## line 1252
+        except TypeError:
+            print("Proper error")
 
 def test_execute():
     st = ScalarUnitTests()   
@@ -147,8 +177,10 @@ def test_execute():
     tt.TestChangeShape()
     tt.TestCreateFromData()
 
+    at.TestArrayGetElement()
     at.TestArrayNumElements()
     at.TestArraySetElement
     at.TestArrayToTensor()
+    at.TestCopyTo()
 
 test_execute()
