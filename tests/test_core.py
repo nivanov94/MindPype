9696cb62fdc62187b70079ebd6bf286eba83782d
Samantha Schaus <samantha.schaus11@gmail.com>
2025-07-07 08:30:24 -0400

    code coverage

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,44 +1,63 @@
 import mindpype as mp
 import numpy as np
-import pickle
 import pytest
 from sklearn.svm import SVC
 
 class CoreUnitTest():
     """Unit test for Session class in core.py"""
-    def __init__(self):
-        self.__session = mp.Session.create()
     
     def TestFindObjFunc(self):
         """Ensure find_obj executes without error and returns correct object using session id"""
+        session = mp.Session.create()
+        graph = mp.Graph.create(session)
+
         raw_data = np.zeros((3,3,3))
 
-        t = mp.Tensor.create_from_data(self.__session, raw_data)
-        s = mp.Scalar.create_from_value(self.__session, 'test')         
-        misc = mp.Classifier.create_SVM(self.__session)
+        t = mp.Tensor.create_from_data(session, raw_data)
+        s = mp.Scalar.create_from_value(session, 'test')         
+        misc = mp.Classifier.create_SVM(session)
+        src = mp.source.InputLSLStream.create_marker_uncoupled_data_stream(session, active=False)
 
-        ## how to do id of session?
+        session_obj = session.find_obj(session.session_id)
+        assert session_obj == session
 
-        t_obj = self.__session.find_obj(t.session_id)
+        t_obj = session.find_obj(t.session_id)
         assert t_obj == t
 
-        s_obj = self.__session.find_obj(s.session_id)  
+        s_obj = session.find_obj(s.session_id)  
         assert s_obj == s
 
-        misc_obj = self.__session.find_obj(misc.session_id)   ## this is returning None
-        print(misc.mp_type)
+        misc_obj = session.find_obj(misc.session_id) 
         assert misc_obj == misc    
 
-        ## can we do ext src?
+        src_obj = session.find_obj(src.session_id)
+        assert src_obj == src
+
+        graph_obj = session.find_obj(graph.session_id)
+        assert graph_obj == None
     
     def TestAddToSession(self):
         """Ensure add_to_session correctly MindPype adds object to session"""
-        ## obj not inheriting MPBase???
+        session = mp.Session.create()
+        graph = mp.Graph.create(session)
 
         test_string = 'test'
-
+        # Only objects inheriting MPBase can be added to a session
         try:
-            self.__session.add_to_session(test_string)
+            session.add_to_session(test_string)
+        except ValueError:
+            pass
+
+        src = mp.source.InputLSLStream.create_marker_uncoupled_data_stream(session, active=False)
+        session.add_to_session(src)
+
+        inA = np.zeros((1,1))
+        inB = np.ones((1,1))
+        out = np.zeros((1,1))
+        kernel = mp.kernels.AdditionKernel(graph, inA, inB, out)
+        # Kernal cannot be directly added to session
+        try:
+            session.add_to_session(kernel)
         except ValueError:
             pass
     
