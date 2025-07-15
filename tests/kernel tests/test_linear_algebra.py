91bc4a410b357aefa53b0e65aabd6d74ac99975e
Samantha Schaus <samantha.schaus11@gmail.com>
2025-07-15 12:02:47 -0400

    code coverage

--- tests/kernel tests/test_linear_algebra.py
+++ tests/kernel tests/test_linear_algebra.py
@@ -13,7 +13,31 @@ class LinearAlgebraUnitTest:
 
         node = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA, inB, out)
 
+        in_data = [mp.Tensor.create_from_data(self.__session, [1,1,1,1]), mp.Tensor.create_from_data(self.__session, [2,2,2,2]), mp.Tensor.create_from_data(self.__session, [3,3,3,3])]
+        labels = mp.Tensor.create_from_data(self.__session, [1,2,3])
+        # labels.data = np.array([1,2,3,4])
+
+        # node = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA, inB, out, init_inputs=in_data, init_labels=labels)
+
         self.__graph.verify()
+        self.__graph.initialize()
+        self.__graph.execute()
+
+
+    def TestComputeOutputSize(self):
+        inA_inner_2 = mp.Tensor.create_from_data(self.__session, [[1,2], [1,2]])
+        inB_inner_2 = mp.Tensor.create_from_data(self.__session, [[1,2,3], [1,2,3]])
+
+        inA_inner_3 = mp.Tensor.create_from_data(self.__session, [[[1,2], [1,2]], [[1,2], [1,2]]])
+        inB_inner_3 = mp.Tensor.create_from_data(self.__session, [[[1,2], [1,2]], [[1,2], [1,2]]])
+
+        out1 = mp.Tensor.create(self.__session, (2, 3))
+
+        n1 = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA_inner_2, inB_inner_2, out1)
+        # n2 = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA_inner_3, inB_inner_2)
+        # n3 = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA_inner_2, inB_inner_3)
+        # n4 = mp.kernels.MatrixMultKernel.add_to_graph(self.__graph, inA_inner_3, inB_inner_3)
+
 
 def test_execute():
     test = LinearAlgebraUnitTest()
